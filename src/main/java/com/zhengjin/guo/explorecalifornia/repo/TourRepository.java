dd66f31567407bac9fbe6df8e84daf22cfd0c545
Guo Zhengjin <guo.zhengjin@gmail.com>
2021-10-23 11:40:26 +0800

    added repositories

---
+++ src/main/java/com/zhengjin/guo/explorecalifornia/repo/TourRepository.java
@@ -0,0 +1,7 @@
+package com.zhengjin.guo.explorecalifornia.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TourRepository extends CrudRepository<Tour, Integer> {
+
+}
