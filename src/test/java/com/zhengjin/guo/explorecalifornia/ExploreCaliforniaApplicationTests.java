8013bb323ea39cec12ea06f5891190499ac4e6f8
Guo Zhengjin <guo.zhengjin@gmail.com>
2021-10-23 10:02:42 +0800

    first commit

---
+++ src/test/java/com/zhengjin/guo/explorecalifornia/ExploreCaliforniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhengjin.guo.explorecalifornia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploreCaliforniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
